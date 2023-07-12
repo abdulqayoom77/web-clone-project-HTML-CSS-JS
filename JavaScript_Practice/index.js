a2ec4bbf1537d8ee88ada196fc7442f08f619709
Abdul Qayoom Aijaz <117985917+abdulqayoom77@users.noreply.github.com>
2023-07-12 20:57:39 +0000

    forLoop

---
+++ JavaScript_Practice/index.js
@@ -0,0 +1,28 @@
+// const prompt = require('prompt-sync')();
+// let a = prompt("What do u need")
+// switch (a) {
+//   case 'oranges':
+//     console.log("oranges are 7$")
+//     break
+//   case 'mangoes':
+//     console.log("mangoes are 32$")
+//     break
+//   case 'apples':
+//     console.log("apples are 3$")
+//     break
+//   case 'potatoes':
+//     console.log("potatoes are 9$")
+//     break
+//   case 'tomatoes':
+//     console.log("tomatoes are 47$")
+//     break
+//   case 'bananas':
+//     console.log("bananas are 56$")
+//     break
+//   default:
+//     console.log("we are out of ", (a))
+
+// }
+
+for (let a=0;a<2000;a++)
+  console.log(a)
