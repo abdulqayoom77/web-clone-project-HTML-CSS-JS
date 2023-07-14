08d5de3cd40af8389236523995793ec2c0bbf066
Abdul Qayoom Aijaz <117985917+abdulqayoom77@users.noreply.github.com>
2023-07-14 18:07:16 +0000

    forLoops

---
+++ for loop/loop.js
@@ -0,0 +1,8 @@
+
+var cleanestCities = ["Cheyenne", "Santa Fe", "Tucson", "Great Falls", "Honolulu"];
+cleanestCities = prompt("Enter your city");
+for (var i = 0; i <= 4; i++) {
+    if (cityToCheck === cleanestCities[i]) {
+    alert("It's one of the cleanest cities");
+    }
+}
