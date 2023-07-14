93405de016e3795b2b67b7471849f59399eb0e8a
Abdul Qayoom Aijaz <117985917+abdulqayoom77@users.noreply.github.com>
2023-07-14 06:02:56 +0000

    nestedifs

---
+++ Nested_Ifs/nested_ifs.js
@@ -0,0 +1,12 @@
+var firstName = "Abdul";
+var lastName = "Qayoom";
+var fName = prompt("Enter first Name");
+var lName = prompt("Enter last Name");
+if (firstName === fName) {
+  if (lastName === lName) {
+    alert("That's Your Name");
+  }
+}
+else {
+    alert("That's not Your Name")
+}
