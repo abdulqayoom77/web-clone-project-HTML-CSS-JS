d5612836354402a8461713a55f8c1d5ae832c74b
Abdul Qayoom Aijaz <117985917+abdulqayoom77@users.noreply.github.com>
2023-07-14 17:53:32 +0000

    else

--- else if stats/index.js
+++ else if stats/index.js
@@ -1,19 +1,48 @@
-alert("Enter Number")
-let num=prompt("Enter number")
-if (num%2==0){
-  console.log("Your number is divisible by 2")
-}
-if (num%3==0){
-  console.log("Your number is divisible by 3")
-}
-if(num%2!=0 && num%3!=0) {
-  console.log("Your number is not divisible by 2 or 3")
-}
+// alert("Enter Number")
+// let num=prompt("Enter number")
+// if (num%2==0){
+//   console.log("Your number is divisible by 2")
+// }
+// if (num%3==0){
+//   console.log("Your number is divisible by 3")
+// }
+// if(num%2!=0 && num%3!=0) {
+//   console.log("Your number is not divisible by 2 or 3")
+// }
 
-else if(num%2!=0){
-  console.log("Your number is not divisible by 2")
-}
-else if(num%3!=0){
-  console.log("Your number is not divisible by 3")
-}
+// else if(num%2!=0){
+//   console.log("Your number is not divisible by 2")
+// }
+// else if(num%3!=0){
+//   console.log("Your number is not divisible by 3")
+// }
 
+let num=prompt("ENTER your number")
+num=Number.parseInt(num)
+  if(num%2!=0 && nu){
+    console.log("Your number is not divisible by 2 or 3")
+  }
+    else if (num%2==0){
+      console.log("Your number is ivisible by 2")
+    }
+else if (num%3==0){
+      console.log("Your number is ivisible by 3")
+    }
+  else if (num%4==0){
+      console.log("Your number is ivisible by 4")
+    }
+    else if (num%5==0){
+      console.log("Your number is ivisible by 5")
+    }
+      else if (num%6==0){
+      console.log("Your number is ivisible by 6")
+    }
+        else if (num%7==0){
+      console.log("Your number is ivisible by 7")
+    }
+        else if (num%8==0){
+      console.log("Your number is ivisible by 8")
+    }
+  else {
+    console.log("your number divisible by 2 or 3")
+  }
