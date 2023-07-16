5c886d112d9aafffbac4a1ccfa40672e4b126d3c
Abdul Qayoom Aijaz <117985917+abdulqayoom77@users.noreply.github.com>
2023-07-16 16:59:09 +0000

    nestedForLoops

---
+++ Nested_for_loops_+_Arrays/index.js
@@ -0,0 +1,26 @@
+// Nested For Loops and Arrays 
+
+// var bigNums = [234, 2142, 325, 2314];
+// var smallNums = [32, 43, 23];
+// var sumNum = [];
+// for (var i = 0; i < bigNums.length; i++) {
+//     for (var j = 0; j < smallNums.length; j++) {
+//         sumNum.push(bigNums[i] + smallNums[j])
+//     }
+// }
+// alert(sumNum);
+
+// Nested For Loops Only
+
+var bigNums = prompt("Enter first Number");
+var smallNums = prompt("Enter first Number");
+var num1 = parseInt(bigNums);
+var num2 = parseInt(smallNums);
+var sumNum = [];
+for (var i = 0; i < bigNums.length; i++) {
+    for (var j = 0; j < smallNums.length; j++) {
+        sumNum.push(bigNums[i] + smallNums[j])
+    }
+}
+alert(sumNum);
+
