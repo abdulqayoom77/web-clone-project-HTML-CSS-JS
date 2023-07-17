ff923c36a14fbc8393ff255372e608a0de57f3f6
Abdul Qayoom Aijaz <117985917+abdulqayoom77@users.noreply.github.com>
2023-07-17 12:00:21 +0000

    upperCase

--- Changing_Case/upperCase.js
+++ Changing_Case/upperCase.js
@@ -1,9 +1,12 @@
-var nickname = "abdul";
-var nickname = nickname.toUpperCase();
-console.log(nickname);
-if (nickname.toUpperCase() !== "abdul") {
-    console.log("Right");
-}
-else{
-    console.log("Wrong")
-}
+// var nickname = "abdul";
+// var nickname = nickname.toUpperCase();
+// console.log(nickname);
+// if (nickname.toUpperCase() !== "abdul") {
+//     console.log("Right");
+// }
+// else{
+//     console.log("Wrong")
+// }
+
+var lastName = prompt("Enter Your Last Name Here:");
+alert(lastName.toUpperCase());
