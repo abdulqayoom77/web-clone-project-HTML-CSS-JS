b49611ca28574d3d87f576dcc1fd42e205f8715d
Abdul Qayoom Aijaz <117985917+abdulqayoom77@users.noreply.github.com>
2023-07-17 11:50:33 +0000

    upperCase

---
+++ Changing_Case/lowerCase.js
@@ -0,0 +1,9 @@
+var nickname = "abdul";
+var nickname = nickname.toUpperCase();
+console.log(nickname);
+if (nickname.toUpperCase() !== "abdul") {
+    console.log("Right");
+}
+else{
+    console.log("Wrong")
+}
