1cbb355bd6153d469cbdc13ec41825a6ce3c8fe3
Abdul Qayoom Aijaz <117985917+abdulqayoom77@users.noreply.github.com>
2023-07-21 16:58:24 +0000

    7-21-2023

--- JS_If_Statements/ifStatements.js
+++ JS_If_Statements/ifStatements.js
@@ -2,7 +2,7 @@ var weight = prompt("Enter weight");
 var time = prompt("Enter time");
 var age = prompt("Enter age");
 var gender = prompt("Enter gender");
-if (weight > 300 && time < 6 && age > 17 && gender === "male") {
+if (weight > 300 && time < 6 && age > 18 && gender === "male") {
     alert("Come to our tryout!");
 }
 else {
