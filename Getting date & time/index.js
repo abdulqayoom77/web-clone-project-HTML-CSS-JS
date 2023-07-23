6f074bcc1cb9fccd46a576f5c7ab2eae2a8d4c11
Abdul Qayoom Aijaz <117985917+abdulqayoom77@users.noreply.github.com>
2023-07-23 12:30:33 +0000

    7/23/2023

--- Getting date & time/index.js
+++ Getting date & time/index.js
@@ -4,3 +4,8 @@ console.log(date);
 
 // Extracting the day of the week
 
+var dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+var now = new Date();
+var theDay = now.getDay();
+var nameOfToday = dayNames[theDay];
+console.log(nameOfToday);
