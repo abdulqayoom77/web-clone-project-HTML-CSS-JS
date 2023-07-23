b6bb1e0baf49c06ff93026ac6f4f7c3f04ecccef
Abdul Qayoom Aijaz <117985917+abdulqayoom77@users.noreply.github.com>
2023-07-23 08:13:06 +0000

    7/23/2023

--- Generating Random Numbers/index.js
+++ Generating Random Numbers/index.js
@@ -1,6 +1,10 @@
 // Generating Random Numbers
 // Creating Dice Function with simple Program
-var bigDecimal = Math.random();
-var improvedNum = (bigDecimal * 6) + 1;
-var numberOfStars = Math.floor(improvedNum);
-console.log(numberOfStars);
+// var bigDecimal = Math.random();
+// var improvedNum = (bigDecimal * 6) + 1;
+// var numberOfStars = Math.floor(improvedNum);
+// console.log(numberOfStars);
+
+var intNum = Math.random();
+var resultNum = Math.floor(intNum * 10) + 1;
+console.log(resultNum);
