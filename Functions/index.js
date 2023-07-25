c756a3846d76c8946044fe55de7bd4ba1fcd6a48
Abdul Qayoom Aijaz <117985917+abdulqayoom77@users.noreply.github.com>
2023-07-25 17:17:33 +0000

    7/25/2023

---
+++ Functions/index.js
@@ -0,0 +1,11 @@
+function tellTime() {
+    var now = new Date();
+    var theHr = now.getHours();
+    var theMin = now.getMinutes();
+    console.log("Current time: " + theHr + ":" + theMin);
+}
+
+// var now = new Date();
+// var theHr = now.getHours();
+// var theMin = now.getMinutes();
+// console.log("Current time: " + theHr + ":" + theMin);
