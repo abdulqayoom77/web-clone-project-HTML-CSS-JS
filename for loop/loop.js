debdee86acccb9e987fa827a66a4dc78955ecdc1
Abdul Qayoom Aijaz <117985917+abdulqayoom77@users.noreply.github.com>
2023-07-26 16:55:25 +0000

    7/26/2023

--- for loop/loop.js
+++ for loop/loop.js
@@ -1,11 +1,27 @@
 
-var cleanestCities = ["Cheyenne", "Santa Fe", "Tucson", "Great Falls", "Honolulu","karachi"];
-cleanestCities = prompt("Enter your city");
-for (var i = 0; i <= 5; i++) {
-    if (cityToCheck === cleanestCities[i]) {
-    alert("It's one of the cleanest cities");
+// var cleanestCities = ["Cheyenne", "Santa Fe", "Tucson", "Great Falls", "Honolulu","karachi"];
+// cleanestCities = prompt("Enter your city");
+// for (var i = 0; i <= 5; i++) {
+//     if (cityToCheck === cleanestCities[i]) {
+//     alert("It's one of the cleanest cities");
+//     }
+// }
+// if (matchFound === false) {
+//     alert("It's not on the list of cleanest cities");
+// }
+
+// Prime Numbers in For loop
+
+var n = 100;
+for (var i = 2; i <= n; i++) {
+    var isPrime = true;
+    for (var j = 2; j <= Math.sqrt(i); j++) { // nested loop
+        if (i % j === 0) {
+            isPrime = false;
+            break;
+        }
     }
-}
-if (matchFound === false) {
-    alert("It's not on the list of cleanest cities");
+    if (isPrime) {
+        console.log(i);
+    }
 }
