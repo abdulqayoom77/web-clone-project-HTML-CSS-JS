557e007847122baaf8e9ad922bc3ea0fb3f06d46
Abdul Qayoom Aijaz <117985917+abdulqayoom77@users.noreply.github.com>
2023-07-27 17:37:53 +0000

    7/27/2023

--- Functions/index.js
+++ Functions/index.js
@@ -10,8 +10,13 @@
 // var theMin = now.getMinutes();
 // console.log("Current time: " + theHr + ":" + theMin);
 
-function onePlusAvg(num1, num2) {
-    var sum = 1 + (num1 + num2) / 2;
-    console.log(sum);
+// function onePlusAvg(num1, num2) {
+//     var sum = 1 + (num1 + num2) / 2;
+//     console.log(sum);
+// }
+// onePlusAvg(2, 9);
+
+function greetUser(greeting) {
+    console.log(greeting);
 }
-onePlusAvg(2, 9);
+greetUser("Hi! I am Abdul");
