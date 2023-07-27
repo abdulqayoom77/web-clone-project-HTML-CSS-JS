445636e67a0cc86135a28117c68e3dcd12aa2e02
Abdul Qayoom Aijaz <117985917+abdulqayoom77@users.noreply.github.com>
2023-07-27 17:24:36 +0000

    7/27/2023

--- Functions/index.js
+++ Functions/index.js
@@ -1,11 +1,17 @@
-function tellTime() {
-    var now = new Date();
-    var theHr = now.getHours();
-    var theMin = now.getMinutes();
-    console.log("Current time: " + theHr + ":" + theMin);
-}
+// function tellTime() {
+//     var now = new Date();
+//     var theHr = now.getHours();
+//     var theMin = now.getMinutes();
+//     console.log("Current time: " + theHr + ":" + theMin);
+// }
 
 // var now = new Date();
 // var theHr = now.getHours();
 // var theMin = now.getMinutes();
 // console.log("Current time: " + theHr + ":" + theMin);
+
+function onePlusAvg(num1, num2) {
+    var sum = 1 + (num1 + num2) / 2;
+    console.log(sum);
+}
+onePlusAvg(2, 9);
