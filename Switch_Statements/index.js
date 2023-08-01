f0ee1ac22189d69a2a33583eb777f6ccdb88c4ec
Abdul Qayoom Aijaz <117985917+abdulqayoom77@users.noreply.github.com>
2023-08-01 17:22:21 +0000

    1/8/2023

---
+++ Switch_Statements/index.js
@@ -0,0 +1,24 @@
+var items = prompt("What do u need").toLowerCase();
+switch (items) {
+    case "mango":
+    alert("Price of mangoes is 150Rs per kg");
+break;
+    case "apple":
+    alert("Price of mangoes is 150Rs per kg");
+break;
+    case "banana":
+    alert("Price of mangoes is 150Rs per kg");
+break;
+    case "strawberry":
+    alert("Price of mangoes is 150Rs per kg");
+break;
+    case "potatoes":
+    alert("Price of mangoes is 150Rs per kg");
+break;
+    case "tomatoes":
+    alert("Price of mangoes is 150Rs per kg");
+break;
+default :
+alert("Type the spelling Correctly \n 1. MANGO \n 2. APPLE");
+}
+document.write();
