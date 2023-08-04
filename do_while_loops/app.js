8e8131bdf408c282ee7dd7b23443d34ddd5ebf2c
Abdul Qayoom Aijaz <117985917+abdulqayoom77@users.noreply.github.com>
2023-08-04 13:37:25 +0000

    4//2023

---
+++ do_while_loops/app.js
@@ -0,0 +1,19 @@
+var animals = ["horse", "ox", "cow", "pig", "duck"];
+var i = 0;
+do {
+  if (animals[i] === "pig") {
+    alert("Found it!");
+    break;
+  }
+  i++;
+} while (i < animals.length);
+
+var animals = ["horse", "ox", "cow", "pig", "duck"];
+var i = 0;
+do {
+  if (animals[i] === "pig") {
+    alert("Found it!");
+    break;
+  }
+  i++;
+} while (i < animals.length);
