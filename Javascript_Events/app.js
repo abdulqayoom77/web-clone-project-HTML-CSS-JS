3719f3c3ad408616dbc6654dfdaf1ba284b48279
Abdul Qayoom Aijaz <117985917+abdulqayoom77@users.noreply.github.com>
2023-08-06 17:39:15 +0000

    6/8/2023

--- Javascript_Events/app.js
+++ Javascript_Events/app.js
@@ -1,3 +1,11 @@
 function event(message) {
     alert(message);
 }
+
+function checkAddress(fieldId) {
+    var val = document.getElementById(fieldId).value;
+    if (val === "") {
+    alert("Email address required.");
+    }
+}
+    
