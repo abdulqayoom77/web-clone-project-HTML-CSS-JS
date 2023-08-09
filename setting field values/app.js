50e92481d176ba0f2d25c10d7d0e5811656833ec
Abdul Qayoom Aijaz <117985917+abdulqayoom77@users.noreply.github.com>
2023-08-09 09:12:46 +0000

    9/8/2023

---
+++ setting field values/app.js
@@ -0,0 +1,15 @@
+function fillCity() {
+    var cityName;
+    var zipEntered = document.getElementById("zip").value;
+    switch (zipEntered) {
+        case "60608":
+            cityName = "Chicago";
+            break;
+        case "68114":
+            cityName = "Omaha";
+            break;
+        case "53212":
+            cityName = "Milwaukee";
+    }
+    document.getElementById("city").value = cityName;
+}
