7a4cbf7ea6c8ae0758b0f670f554b1539d6e6a87
Abdul Qayoom Aijaz <117985917+abdulqayoom77@users.noreply.github.com>
2023-08-10 17:49:34 +0000

    10/8/2023

--- setting field values/app.js
+++ setting field values/app.js
@@ -1,15 +1,36 @@
-function fillCity() {
-    var cityName;
-    var zipEntered = document.getElementById("zip").value;
-    switch (zipEntered) {
-        case "60608":
-            cityName = "Chicago";
-            break;
-        case "68114":
-            cityName = "Omaha";
-            break;
-        case "53212":
-            cityName = "Milwaukee";
+// function fillCity() {
+//     var cityName;
+//     var zipEntered = document.getElementById("zip").value;
+//     switch (zipEntered) {
+//         case "60608":
+//             cityName = "Chicago";
+//             break;
+//         case "68114":
+//             cityName = "Omaha";
+//             break;
+//         case "53212":
+//             cityName = "Milwaukee";
+//     }
+//     document.getElementById("city").value = cityName;
+// }
+function amountTocoins(amount, coins) 
+{
+ if (amount === 0) 
+  {
+     return [];
+   } 
+ else
+   {
+     if (amount >= coins[0]) 
+       {
+        left = (amount - coins[0]);
+        return [coins[0]].concat( amountTocoins(left, coins) );
+        } 
+      else
+        {
+         coins.shift();
+         return amountTocoins(amount, coins);
+        }
     }
-    document.getElementById("city").value = cityName;
-}
+} 
+document.write(amountTocoins(46, [25, 10, 5, 2,1]));
