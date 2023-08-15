5f7936f5da4ad6bbe7211ca54b8135e94d4c2e12
Abdul Qayoom Aijaz <117985917+abdulqayoom77@users.noreply.github.com>
2023-08-15 11:54:43 +0000

    15/8/2023

--- Nested_for_loops_+_Arrays/index.js
+++ Nested_for_loops_+_Arrays/index.js
@@ -34,4 +34,17 @@
 // }
 // console.log(fullNames);
 
-for (var i=)
+// var n = 100;
+// for (var i = 2; i <= n; i++) {
+//     var isPrime = true;
+//     for (var j = 2; j <= Math.sqrt(i); j++) { // nested loop
+//         if (i % j === 0) {
+//             isPrime = false;
+//             break;
+//         }
+//     }
+//     if (isPrime) {
+//         console.log(i);
+//     }
+// }
+
