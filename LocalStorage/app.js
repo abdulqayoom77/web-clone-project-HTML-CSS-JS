e4805d830f6e0755b91ed6cbe6ba59a49c53c74f
Abdul Qayoom Aijaz <117985917+abdulqayoom77@users.noreply.github.com>
2023-09-02 17:19:30 +0000

    2/9/2023

--- LocalStorage/app.js
+++ LocalStorage/app.js
@@ -1,6 +1,13 @@
 function signup(){
+
     var email = document.getElementById('semail').value
     var pass = document.getElementById('spass').value
+    // var obj = {
+    //     email : 'Email',
+    //     pass : 'Password'
+    // }
+    // var obj2 = JSON.stringify(obj)
+    // obj2 = JSON.parse(obj)
     localStorage.setItem('Email',email)
     localStorage.setItem('Password',pass)
     location.href = './login.html'
@@ -16,4 +23,9 @@ function signin() {
         alert('sin up first')
         location.href = './signup.html';
     }
+}
+
+function logout(){
+    // localStorage.clear();
+    location.href = './login.html'
 }
