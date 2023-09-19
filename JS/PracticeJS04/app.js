844d2c89074fa0c02bf30ea86ddcdb42ab551bd7
Abdul Qayoom Aijaz <117985917+abdulqayoom77@users.noreply.github.com>
2023-09-19 10:38:46 +0000

    19/9/2023

---
+++ JS/PracticeJS04/app.js
@@ -0,0 +1,328 @@
+let myJokes = [
+
+  {
+    category: "Programming",
+    type: "single",
+    joke: "Debugging is like being the detective in a crime movie where you're also the murderer at the same time.",
+    flags: {
+      nsfw: false,
+      religious: false,
+      political: false,
+      racist: false,
+      sexist: false,
+      explicit: false,
+    },
+    id: 42,
+    safe: true,
+    lang: "en",
+  },
+  {
+    category: "Programming",
+    type: "single",
+    joke: "Java and C were telling jokes. It was C's turn, so he writes something on the wall, points to it and says \"Do you get the reference?\" But Java didn't.",
+    flags: {
+      nsfw: false,
+      religious: false,
+      political: false,
+      racist: false,
+      sexist: false,
+      explicit: false,
+    },
+    id: 4,
+    safe: true,
+    lang: "en",
+  },
+  {
+    category: "Programming",
+    type: "single",
+    joke: 'A man is smoking a cigarette and blowing smoke rings into the air. His girlfriend becomes irritated with the smoke and says "Can\'t you see the warning on the cigarette pack? Smoking is hazardous to your health!" to which the man replies, "I am a programmer.  We don\'t worry about warnings; we only worry about errors."',
+    flags: {
+      nsfw: false,
+      religious: false,
+      political: false,
+      racist: false,
+      sexist: false,
+      explicit: false,
+    },
+    id: 38,
+    safe: true,
+    lang: "en",
+  },
+  {
+    category: "Programming",
+    type: "single",
+    joke: "The glass is neither half-full nor half-empty, the glass is twice as big as it needs to be.",
+    flags: {
+      nsfw: false,
+      religious: false,
+      political: false,
+      racist: false,
+      sexist: false,
+      explicit: false,
+    },
+    id: 23,
+    safe: true,
+    lang: "en",
+  },
+  {
+    category: "Programming",
+    type: "single",
+    joke: "Your mama's so FAT she can't save files bigger than 4GB.",
+    flags: {
+      nsfw: false,
+      religious: false,
+      political: false,
+      racist: false,
+      sexist: false,
+      explicit: true,
+    },
+    id: 9,
+    safe: false,
+    lang: "en",
+  },
+  {
+    category: "Programming",
+    type: "single",
+    joke: 'Eight bytes walk into a bar.\nThe bartender asks, "Can I get you anything?"\n"Yeah," reply the bytes.\n"Make us a double."',
+    flags: {
+      nsfw: false,
+      religious: false,
+      political: false,
+      racist: false,
+      sexist: false,
+      explicit: false,
+    },
+    id: 34,
+    safe: true,
+    lang: "en",
+  },
+  {
+    category: "Programming",
+    type: "single",
+    joke: "Algorithm: A word used by programmers when they don't want to explain how their code works.",
+    flags: {
+      nsfw: false,
+      religious: false,
+      political: false,
+      racist: false,
+      sexist: false,
+      explicit: false,
+    },
+    id: 51,
+    safe: true,
+    lang: "en",
+  },
+  {
+    category: "Programming",
+    type: "single",
+    joke: "// This line doesn't actually do anything, but the code stops working when I delete it.",
+    flags: {
+      nsfw: false,
+      religious: false,
+      political: false,
+      racist: false,
+      sexist: false,
+      explicit: false,
+    },
+    id: 12,
+    safe: true,
+    lang: "en",
+  },
+  {
+    category: "Programming",
+    type: "single",
+    joke: "Documentation is like sex:\nWhen it's good, it's very good.\nWhen it's bad, it's better than nothing...",
+    flags: {
+      nsfw: true,
+      religious: false,
+      political: false,
+      racist: false,
+      sexist: false,
+      explicit: false,
+    },
+    safe: false,
+    id: 305,
+    lang: "en",
+  },
+  {
+    category: "Programming",
+    type: "single",
+    joke: "UDP is better in the COVID era since it avoids unnecessary handshakes.",
+    flags: {
+      nsfw: false,
+      religious: false,
+      political: false,
+      racist: false,
+      sexist: false,
+      explicit: false,
+    },
+    id: 259,
+    safe: true,
+    lang: "en",
+  },
+  {
+    "category": "Programming",
+    "type": "single",
+    "joke": "A man is smoking a cigarette and blowing smoke rings into the air. His girlfriend becomes irritated with the smoke and says \"Can't you see the warning on the cigarette pack? Smoking is hazardous to your health!\" to which the man replies, \"I am a programmer.  We don't worry about warnings; we only worry about errors.\"",
+    "flags": {
+      "nsfw": false,
+      "religious": false,
+      "political": false,
+      "racist": false,
+      "sexist": false,
+      "explicit": false
+    },
+    "id": 38,
+    "safe": true,
+    "lang": "en"
+  },
+  {
+    "category": "Programming",
+    "type": "single",
+    "joke": "Judge: \"I sentence you to the maximum punishment...\"\nMe (thinking): \"Please be death, please be death...\"\nJudge: \"Learn Java!\"\nMe: \"Damn.\"",
+    "flags": {
+      "nsfw": false,
+      "religious": false,
+      "political": false,
+      "racist": false,
+      "sexist": false,
+      "explicit": false
+    },
+    "id": 45,
+    "safe": true,
+    "lang": "en"
+  },
+  {
+    "category": "Programming",
+    "type": "single",
+    "joke": "Algorithm: A word used by programmers when they don't want to explain how their code works.",
+    "flags": {
+      "nsfw": false,
+      "religious": false,
+      "political": false,
+      "racist": false,
+      "sexist": false,
+      "explicit": false
+    },
+    "id": 51,
+    "safe": true,
+    "lang": "en"
+  },
+  {
+    "category": "Programming",
+    "type": "single",
+    "joke": "Two SQL tables sit at the bar. A query approaches and asks \"Can I join you?\"",
+    "flags": {
+      "nsfw": false,
+      "religious": false,
+      "political": false,
+      "racist": false,
+      "sexist": false,
+      "explicit": false
+    },
+    "id": 221,
+    "safe": true,
+    "lang": "en"
+  },
+  {
+    "category": "Programming",
+    "type": "single",
+    "joke": "A SQL statement walks into a bar and sees two tables.\nIt approaches, and asks \"may I join you?\"",
+    "flags": {
+      "nsfw": false,
+      "religious": false,
+      "political": false,
+      "racist": false,
+      "sexist": false,
+      "explicit": false
+    },
+    "id": 5,
+    "safe": true,
+    "lang": "en"
+  },
+  {
+    "category": "Programming",
+    "type": "single",
+    "joke": "Today I learned that changing random stuff until your program works is \"hacky\" and a \"bad coding practice\" but if you do it fast enough it's \"Machine Learning\" and pays 4x your current salary.",
+    "flags": {
+      "nsfw": false,
+      "religious": false,
+      "political": false,
+      "racist": false,
+      "sexist": false,
+      "explicit": false
+    },
+    "id": 33,
+    "safe": true,
+    "lang": "en"
+  },
+  {
+    "category": "Programming",
+    "type": "single",
+    "joke": "How do you tell HTML from HTML5?\n- Try it out in Internet Explorer\n- Did it work?\n- No?\n- It's HTML5.",
+    "flags": {
+      "nsfw": false,
+      "religious": false,
+      "political": false,
+      "racist": false,
+      "sexist": false,
+      "explicit": false
+    },
+    "id": 43,
+    "safe": true,
+    "lang": "en"
+  },
+  {
+    "category": "Programming",
+    "type": "single",
+    "joke": "A byte walks into a bar looking miserable.\nThe bartender asks it: \"What's wrong buddy?\"\n\"Parity error.\" it replies. \n\"Ah that makes sense, I thought you looked a bit off.\"",
+    "flags": {
+      "nsfw": false,
+      "religious": false,
+      "political": false,
+      "racist": false,
+      "sexist": false,
+      "explicit": false
+    },
+    "id": 24,
+    "safe": true,
+    "lang": "en"
+  },
+  {
+    "category": "Programming",
+    "type": "single",
+    "joke": "UDP is better in the COVID era since it avoids unnecessary handshakes.",
+    "flags": {
+      "nsfw": false,
+      "religious": false,
+      "political": false,
+      "racist": false,
+      "sexist": false,
+      "explicit": false
+    },
+    "id": 259,
+    "safe": true,
+    "lang": "en"
+  },
+  {
+    "category": "Programming",
+    "type": "single",
+    "joke": "\"Honey, go to the store and buy some eggs.\"\n\"OK.\"\n\"Oh and while you're there, get some milk.\"\nHe never returned.",
+    "flags": {
+      "nsfw": false,
+      "religious": false,
+      "political": false,
+      "racist": false,
+      "sexist": false,
+      "explicit": false
+    },
+    "id": 18,
+    "safe": true,
+    "lang": "en"
+  }
+]
+setInterval(function(){
+let index = Math.floor(Math.random() * (myJokes.length - 1))
+var jokeDiv = document.getElementById("div");
+jokeDiv.innerHTML = myJokes[index].joke;
+},10000)
